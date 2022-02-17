7bc7292e4b51e0bb65e3fe162930cb2f61636167
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-02-17 20:58:46 +0900

    [Bronze I] Title: Yangjojang of The Year, Time: 64 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/11557.Yangjojang of The Year/Yangjojang of The Year.py
@@ -0,0 +1,12 @@
+a=input()
+for i in range(int(a)):
+    b=input()
+    n=0
+    for j in range(int(b)):
+        c,d=input().split()
+        if int(d)>int(n):
+            n=d
+            m=c
+    print (m)
+                
+        
