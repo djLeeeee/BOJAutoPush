3dfee26cddbdd97cd245131f5d4c6e8fdfe2b914
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-02-28 12:12:34 +0900

    [Gold II] Title: 트리의 순회, Time: 324 ms, Memory: 166236 KB -BaekjoonHub

--- 백준/Gold/2263.트리의 순회/트리의 순회.py
+++ 백준/Gold/2263.트리의 순회/트리의 순회.py
@@ -1,12 +1,15 @@
 from sys import stdin as s
 from sys import setrecursionlimit as st
 
-st(10 ** 5)
+st(10 ** 6)
 
 input = s.readline
 
 n = int(input())
 in_order = input().split()
+position = {}
+for i in range(n):
+    position[in_order[i]] = i
 first_order = input().split()
 
 
@@ -14,7 +17,7 @@ def make_free(io_s, io_e, fo_s, fo_e):
     if io_s > io_e or fo_s > fo_e:
         return
     print(first_order[fo_e], end=' ')
-    idx = in_order.index(first_order[fo_e])
+    idx = position[first_order[fo_e]]
     make_free(io_s, idx - 1, fo_s, idx - io_s + fo_s - 1)
     make_free(idx + 1, io_e, fo_e - io_e + idx, fo_e - 1)
 
