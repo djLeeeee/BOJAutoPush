bcf65ec54c468049b288f7612eedafc9732e5cc1
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-03-14 02:46:04 +0900

    [Platinum V] Title: 주유소, Time: 7492 ms, Memory: 235016 KB -BaekjoonHub

---
+++ 백준/Platinum/13308. 주유소/주유소.py
@@ -0,0 +1,44 @@
+from sys import stdin
+import heapq
+
+input = stdin.readline
+
+n, m = map(int, input().split())
+oil_rate = [0] + list(map(int, input().split()))
+graph = {}
+for _ in range(m):
+    x, y, d = map(int, input().split())
+    if graph.get(x):
+        graph[x][y] = d
+    else:
+        graph[x] = {y: d}
+    if graph.get(y):
+        graph[y][x] = d
+    else:
+        graph[y] = {x: d}
+INF = float('inf')
+ans = [[INF] * 2501 for _ in range(n + 1)]
+check = [[] for _ in range(n + 1)]
+starts = [(0, oil_rate[1], 1)]
+while starts:
+    total, oil, now = heapq.heappop(starts)
+    if now == n:
+        break
+    if total > ans[now][oil]:
+        continue
+    flag = False
+    if check[now]:
+        for t, o in check[now]:
+            if t >= total and o >= oil:
+                flag = True
+                break
+    else:
+        check[now].append((total, oil))
+    if flag:
+        continue
+    oil = min(oil, oil_rate[now])
+    for adj in graph[now]:
+        if oil * graph[now][adj] + total < ans[adj][oil]:
+            ans[adj][oil] = oil * graph[now][adj] + total
+            heapq.heappush(starts, (ans[adj][oil], oil, adj))
+print(total)
