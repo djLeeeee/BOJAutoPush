113a6e0a2090574e4445c67e4d55f0f3e1c13bb0
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-03-15 02:58:14 +0900

    [Silver I] Title: 숨바꼭질, Time: 76 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,13 @@
+from sys import stdin as s
+def find(n, k):
+    if n >= k:
+        return n - k
+    elif k == 1:
+        return 1
+    elif k % 2:
+        return min ( find(n, k + 1), find(n, k - 1)) +1     
+    else:
+        return min ( k - n, find(n, k // 2) + 1)
+
+a, b = map(int, s.readline().split() )
+print(find(a, b))
