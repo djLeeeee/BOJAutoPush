afb03a8d3e00874ef699235711d7b5eecfab8d9f
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-03-27 22:49:51 +0900

    [Platinum III] Title: 호텔 관리, Time: 1072 ms, Memory: 130672 KB -BaekjoonHub

--- 백준/Platinum/16915. 호텔 관리/호텔 관리.py
+++ 백준/Platinum/16915. 호텔 관리/호텔 관리.py
@@ -12,22 +12,13 @@ def add_edge(a, b):
 
 
 def dfs(idx):
-    if visited[idx]:
-        return
-    visited[idx] = True
-    for adj in graph[idx]:
-        if not visited[adj]:
-            dfs(adj)
-    stack.append(idx)
-
-
-def dfs_inv(idx):
     if scc[idx]:
         return
     scc[idx] = component
     for adj in graph[idx]:
         if not scc[adj]:
-            dfs_inv(adj)
+            dfs(adj)
+    stack.append(idx)
 
 
 def check(arr, l):
@@ -52,17 +43,13 @@ for ri in range(1, n + 1):
     else:
         add_edge(x, -y)
 stack = []
-visited = [False] * (2 * m + 1)
-for i in range(1, m + 1):
-    if not visited[i]:
-        dfs(i)
-    if not visited[-i]:
-        dfs(-i)
 scc = [0] * (2 * m + 1)
 component = 0
-while stack:
-    now = stack.pop()
-    if not scc[now]:
+for i in range(1, m + 1):
+    if not scc[i]:
         component += 1
-        dfs_inv(now)
+        dfs(i)
+    if not scc[-i]:
+        component += 1
+        dfs(-i)
 print(check(scc, m))
