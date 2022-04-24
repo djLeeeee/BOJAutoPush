7ee65c1bb30bb393887650256d35a99a00695c93
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-04-25 02:07:55 +0900

    [Gold V] Title: MooTube (Silver), Time: 96 ms, Memory: 31860 KB -BaekjoonHub

---
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.py
@@ -0,0 +1,48 @@
+from sys import stdin as s
+
+m, n = map(int, s.readline().split())
+usado = []
+for i in range(m - 1):
+    p, q, r = map(int, s.readline().split())
+    usado.append((r, p, q))
+usado.sort(reverse=True)
+limit = []
+origin = []
+for j in range(n):
+    k, v = map(int, s.readline().split())
+    limit.append((k, j))
+    origin.append(v)
+limit.sort()
+parent = list(range(m + 1))
+parent_num = [1] * (m + 1)
+
+
+def find(target):
+    if target == parent[target]:
+        return target
+    parent[target] = find(parent[target])
+    return parent[target]
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+result = [0] * n
+i = 0
+while limit:
+    limit_now, limit_index = limit.pop()
+    while i <= m - 2 and usado[i][0] >= limit_now:
+        x = usado[i][1]
+        y = usado[i][2]
+        total = parent_num[find(x)] + parent_num[find(y)]
+        union(x, y)
+        parent_num[find(x)] = total
+        i += 1
+    result[limit_index] = parent_num[find(origin[limit_index])] - 1
+print(*result, sep='\n')
