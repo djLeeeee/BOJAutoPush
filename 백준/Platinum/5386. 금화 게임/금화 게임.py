7aa95d1655dd9c05325de066b769e31861a7cd52
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-05-01 01:01:10 +0900

    [Platinum IV] Title: 금화 게임, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Platinum/5386. 금화 게임/금화 게임.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+input = stdin.readline
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    if m % 2 or n < m:
+        if n % 2:
+            print(1)
+        else:
+            print(0)
+    else:
+        n %= (m + 1)
+        if n == m:
+            print(m)
+        elif n % 2:
+            print(1)
+        else:
+            print(0)
