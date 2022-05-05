f41bcfaa537722bf0c1da4c133cc46d9ee4a8ab5
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-05-05 11:09:22 +0900

    [Platinum I] Title: 중복 없는 님 게임, Time: 396 ms, Memory: 102948 KB -BaekjoonHub

--- 백준/Platinum/16889. 중복 없는 님 게임/중복 없는 님 게임.py
+++ 백준/Platinum/16889. 중복 없는 님 게임/중복 없는 님 게임.py
@@ -5,14 +5,10 @@ input = stdin.readline
 _ = input()
 nums = list(map(int, input().split()))
 gn = 0
-dp = [0] * (10 ** 6 + 1)
-n = 1
-pointer = 0
-while pointer + n <= 10 ** 6:
-    for dn in range(n):
-        dp[pointer + dn] = n - 1
-    pointer += n
-    n += 1
+dp = [
+    0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7,
+    7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10
+]
 for num in nums:
     gn ^= dp[num]
 print('koosaga' if gn else 'cubelover')
