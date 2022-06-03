88b53e90a2442d6e85f9693be4ed1fe634e2f361
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-06-03 23:34:11 +0900

    [Platinum III] Title: 2-SAT - 4, Time: 284 ms, Memory: 54204 KB -BaekjoonHub

--- 백준/Platinum/11281. 2－SAT － 4/2－SAT － 4.py
+++ 백준/Platinum/11281. 2－SAT － 4/2－SAT － 4.py
@@ -1,5 +1,4 @@
 from sys import stdin, setrecursionlimit
-from collections import defaultdict
 
 input = stdin.readline
 setrecursionlimit(10 ** 5)
@@ -19,33 +18,22 @@ def dfs_inv(idx):
     if scc[idx]:
         return
     scc[idx] = component
-    for adj in graph_inv[idx]:
-        if not scc[adj]:
-            dfs_inv(adj)
-
-
-def sol(array):
-    nn = len(array) // 2
-    result = []
-    for ii in range(1, nn + 1):
-        if array[ii] == array[-ii]:
-            return 0
-        if array[ii] < array[-ii]:
-            result.append(0)
-        else:
-            result.append(1)
-    return result
+    if scc[-idx] == component:
+        print(0)
+        exit()
+    elif idx > 0 and scc[-idx] > 0:
+        ans[idx] = 1
+    for adj in graph[-idx]:
+        if not scc[-adj]:
+            dfs_inv(-adj)
 
 
 n, m = map(int, input().split())
-graph = defaultdict(list)
-graph_inv = defaultdict(list)
+graph = [[] for _ in range(2 * n + 1)]
 for _ in range(m):
     x, y = map(int, input().split())
     graph[-x].append(y)
     graph[-y].append(x)
-    graph_inv[x].append(-y)
-    graph_inv[y].append(-x)
 stack = []
 visited = [False] * (2 * n + 1)
 for i in range(1, n + 1):
@@ -55,14 +43,11 @@ for i in range(1, n + 1):
         dfs(-i)
 scc = [0] * (2 * n + 1)
 component = 0
+ans = [0] * (n + 1)
 while stack:
     now = stack.pop()
     if not scc[now]:
         component += 1
         dfs_inv(now)
-ans = sol(scc)
-if ans:
-    print(1)
-    print(*ans)
-else:
-    print(0)
+print(1)
+print(*ans[1:])
