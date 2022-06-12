4ebc22fc3e7b1b2349407bb5284d2c3ed53d4d21
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-06-12 23:23:53 +0900

    [Bronze IV] Title: Congruent Numbers, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/15128. Congruent Numbers/Congruent Numbers.py
+++ 백준/Bronze/15128. Congruent Numbers/Congruent Numbers.py
@@ -1,6 +1,2 @@
-from sys import stdin
-
-input = stdin.readline
-
-p1, q1, p2, q2 = map(int, input().split())
-print(0 if (p1 * p2) % (q1 * q2 * 2) else 1)
+a,b,c,d=map(int,input().split())
+print(int(a*c%(2*b*d)==0))
