5aa5beae85f3cb21a507135795a88021fe8070a0
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-06-14 23:26:37 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 172 ms, Memory: 32908 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,31 +1,31 @@
-from sys import stdin as s
-from math import inf
+from sys import stdin
 import heapq
 
-input = s.readline
-t = 0
+input = stdin.readline
+
+tc = 0
+INF = float('inf')
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 while True:
     n = int(input())
     if n == 0:
         break
-    t += 1
-    dx = [1, -1, 0, 0]
-    dy = [0, 0, 1, -1]
-    labyrinth = []
-    for i in range(n):
-        labyrinth.append(list(map(int, input().split())))
-    d = [[inf] * n for _ in range(n)]
-    d[0][0] = 0
-    heap = []
-    heapq.heappush(heap, (0, 0, 0))
-    while heap:
-        cost, x, y = heapq.heappop(heap)
-        if cost > d[x][y]:
+    tc += 1
+    board = [list(map(int, input().split())) for _ in range(n)]
+    dist = [[INF] * n for _ in range(n)]
+    dist[0][0] = board[0][0]
+    point = [(dist[0][0], 0, 0)]
+    while point:
+        c, x, y = heapq.heappop(point)
+        if x == y == n - 1:
+            break
+        if dist[x][y] < c:
             continue
-        for j in range(4):
-            nx = x + dx[j]
-            ny = y + dy[j]
-            if 0 <= nx < n and 0 <= ny < n and d[nx][ny] > cost + labyrinth[nx][ny]:
-                d[nx][ny] = cost + labyrinth[nx][ny]
-                heapq.heappush(heap, (d[nx][ny], nx, ny))
-    print(f'Problem {t}: {d[-1][-1] + labyrinth[0][0]}')
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if 0 <= nx < n and 0 <= ny < n and c + board[nx][ny] < dist[nx][ny]:
+                dist[nx][ny] = c + board[nx][ny]
+                heapq.heappush(point, (dist[nx][ny], nx, ny))
+    print(f"Problem {tc}: {c}")
