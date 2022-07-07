488723e5aba7102b9e4f7246fec3dd6ba0421777
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-07-07 10:44:04 +0900

    [Gold III] Title: 세 용액, Time: 156 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -0,0 +1,25 @@
+n = int(input())
+liquid = list(map(int, input().split()))
+liquid.sort()
+
+combination = abs(sum(liquid[:3]))
+result = [0, 1, 2]
+
+for i in range(n - 2):
+    x = i + 1
+    y = n - 1
+    while x != y:
+        my_sum = liquid[i] + liquid[x] + liquid[y]
+        if abs(my_sum) < combination:
+            result = [i, x, y]
+            combination = abs(my_sum)
+        if my_sum < 0:
+            x += 1
+        elif my_sum > 0:
+            y -= 1
+        else:
+            print(liquid[i], liquid[x], liquid[y])
+            exit(0)
+
+for j in result:
+    print(liquid[j], end=' ')
