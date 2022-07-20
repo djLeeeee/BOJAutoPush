578398c98fe6169def24a9ea8abcb04f0a97465e
bomul1128 <97663863+djLeeeee@users.noreply.github.com>
2022-07-21 00:26:09 +0900

    [Platinum II] Title: 행렬 게임, Time: 376 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Platinum/16881. 행렬 게임/행렬 게임.py
+++ 백준/Platinum/16881. 행렬 게임/행렬 게임.py
@@ -1,16 +1,12 @@
-from sys import stdin
-
-input = stdin.readline
-
 n, m = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(n)]
 gn = 0
 for i in range(n):
+    line = list(map(int, input().split()))
     ex = 0
     for j in range(m - 1, -1, -1):
-        if board[i][j] > ex:
-            ex = board[i][j]
+        if line[j] > ex:
+            ex = line[j]
         else:
-            ex = board[i][j] - 1
+            ex = line[j] - 1
     gn ^= ex
 print("koosaga" if gn else "cubelover")
